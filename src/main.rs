1243e9c9bdcbf4ba51a7014d3c8d23a338603a59
Raphael Berger <raphaelmb@gmail.com>
2023-03-06 17:55:31 -0300

    handle arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use text_colorizer::*;
+use std::env;
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Arguments {
+    pattern: String,
+    replace: String,
+    input_file: String,
+    output_file: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() != 4 {
+        print_help();
+        eprintln!("{} wrong number of arguments given. Expected 4, got {}", "Error".red().bold(), args.len());
+        std::process::exit(1);
+    }
+}
+
+fn print_help() {
+    eprintln!("{} - replace a string with a new string", "Find an replace".green());
+    eprintln!("Usage: <target string> <replacement string> <INPUT FILE> <OUTPUT FILE>")
 }
